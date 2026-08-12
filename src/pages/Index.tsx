648517e666087d717a2600eaf1ddc42dc7dbc792
hayz0622 <121319217+hayz0622@users.noreply.github.com>
2026-08-11 22:01:51 -0400

    Shorten repetitive home page sections

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,6 @@ import Navigation from "@/components/Navigation";
 import Hero from "@/components/Hero";
 import FeaturedProjects from "@/components/home/FeaturedProjects";
 import HowItWorks from "@/components/HowItWorks";
-import Features from "@/components/Features";
 import CTA from "@/components/CTA";
 import Footer from "@/components/Footer";
 
@@ -13,7 +12,6 @@ const Index = () => {
       <Hero />
       <FeaturedProjects />
       <HowItWorks />
-      <Features />
       <CTA />
       <Footer />
     </div>
